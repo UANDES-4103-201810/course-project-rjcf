b708430871efe755ae47754064bcf58154329de4
Javier <jahuerta1@miuandes.cl>
2018-04-13 19:41:13 -0300

    funcionando

--- app/models/fund_type.rb
+++ app/models/fund_type.rb
@@ -1,6 +1,6 @@
 class FundType < ApplicationRecord
 	has_many :funds
-	has_many :user through:funds
+	has_many :user, through: :funds
 
 	validates :type, :amount, :username, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 	has_many :funds_types, through: :funds
 	has_many :admins_projects
         has_many :user_projects, through: :admins_projects
-	has_many :paymen_methods, dependent: :delete_all, validate: false
+	has_many :paymen_methods
 
 	validates :name, :username, :biography, :address, presence: true
 	validates :email, presence: true, format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/}
