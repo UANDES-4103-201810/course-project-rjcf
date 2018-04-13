c7f6efb2665fce74da37ba96393ccf09246106fa
rodrigoghersi <righersi@miuandes.cl>
2018-04-13 17:15:23 -0300

    Static Page and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 	def hello
-		render html: "Hello, world"
+		render html: "Welcome to JRCrowdFunding!
+
+		       			Founders: - Rodrigo Ghersi
+
+						  - Javier Huerta"
 	end
 end
