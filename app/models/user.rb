ec720eccc0c178f71e378c6575e87a9bb3fa3229
rodrigoghersi <righersi@miuandes.cl>
2018-04-13 15:22:40 -0300

    Validations*

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
 
 	validates :name, :username, :biography, :address, presence: true
 	validates :email, presence: true, format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/}
-  	validates :password, format: {with: /\A[a-zA-Z0-9\.]{8,12}\z/ , message: "must be between 8 to 12 alphanumeric characters"}
+  	validates :password, format: {with: /\A[a-zA-Z0-9\.]{8,12}\z/ , message: " must be between 8 to 12 alphanumeric characters"}
 end	
