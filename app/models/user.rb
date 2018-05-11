47fc05f0b1cff2692571ae826113b455cad9d755
rodrigoghersi <righersi@miuandes.cl>
2018-05-11 14:13:14 -0300

    arreglando problemas entrega 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :name, :username, :biography, :address, presence: true
+
   validates :email, presence: true, format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/}
-  validates :password, format: {with: /\A[a-zA-Z0-9\.]{8,12}\z/ , message: "must be between 8 to 12 alphanumeric characters"}
+
 end
